a70cdac5f69afb92a1b679d740984f229cf1cf74
joshroderick18 <joshroderick18@gmail.com>
2023-12-17 16:43:34 -0300

    Add some parameters

--- lib/core/services/http_service.dart
+++ lib/core/services/http_service.dart
@@ -2,8 +2,12 @@ import 'package:dio/dio.dart';
 
 class HttpService {
   final _dio = Dio();
+  String baseUrl = 'https://rickandmortyapi.com/api';
 
-  HttpService();
+  HttpService() {
+    _dio.options.baseUrl = baseUrl;
+    _dio.options.headers["Content-Type"] = "application/json";
+  }
 
   Dio get dio => _dio;
 }
